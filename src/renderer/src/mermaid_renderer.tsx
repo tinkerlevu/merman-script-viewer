9f2dfed51b9b8244f98a8d4b1e9f67e0d9b1bcd2
Yo Mom <mail@chocoblu.com>
2026-04-04 16:54:52 +1200

    got mermaid working

--- src/renderer/src/mermaid_renderer.tsx
+++ src/renderer/src/mermaid_renderer.tsx
@@ -1,7 +1,8 @@
 // import './assets/main.css'
 // import './assets/base.css'
 
-import { StrictMode, useEffect } from 'react'
+import mermaid from 'mermaid'
+import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
@@ -15,10 +16,36 @@ function Renderer(): React.JSX.Element {
 
   return (
     <>
-      <br /> popoptpptptptptsptp
+      <div></div>
     </>
   )
 }
+
+const test_def = 'graph TB\na-->b'
+
+const MermaidChart = ({ chart }) => { // TODO: add type variable for ipccallback
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    if (divRef.current) {
+      // Generate unique ID for rendering
+      const id = `mermaid-${Math.random().toString(36).substr(2, 9)}`;
+
+      // Render the diagram
+      mermaid.render(id, chart).then(({ svg }) => {
+        if (divRef.current) {
+          divRef.current.innerHTML = svg;
+        }
+        // WARNING: change the channel on this send:
+        window.electron.ipcRenderer.send('renderer_initalized', svg)
+      });
+    }
+  }, [chart]);
+
+  return <div ref={divRef} className="mermaid"></div>;
+};
+
+
 
 
 
@@ -26,6 +53,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     hello worold lsdkfjsdlfkjsdj
     <Renderer />
+    <MermaidChart chart={test_def} />
   </StrictMode>
 )
 
